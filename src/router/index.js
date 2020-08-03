ced9336d095ea713a3a8347a23fbbe8b20b4700d
zhufangzhou <zhufangzhou@sinovoice.com.cn>
2020-08-03 15:30:10 +0800

    魔法chunkName

--- src/router/index.js
+++ src/router/index.js
@@ -31,150 +31,150 @@ import Layout from '@/layout'
  * all roles can be accessed
  */
 export const constantRoutes = [{
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
+  path: '/login',
+  component: () => import(/* webpackChunkName: "chunk-login" */ '@/views/login/index'),
+  hidden: true
+},
 
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
+{
+  path: '/404',
+  component: () => import(/* webpackChunkName: "chunk-404" */ '@/views/404'),
+  hidden: true
+},
 
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: {
-        title: 'Dashboard',
-        icon: 'dashboard'
-      }
-    }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
+{
+  path: '/',
+  component: Layout,
+  redirect: '/dashboard',
+  children: [{
+    path: 'dashboard',
+    name: 'Dashboard',
+    component: () => import(/* webpackChunkName: "chunk-dashboard" */ '@/views/dashboard/index'),
     meta: {
-      title: 'Example',
-      icon: 'el-icon-s-help'
-    },
-    children: [{
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: {
-          title: 'Table',
-          icon: 'table'
-        }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: {
-          title: 'Tree',
-          icon: 'tree'
-        }
-      }
-    ]
-  },
+      title: 'Dashboard',
+      icon: 'dashboard'
+    }
+  }]
+},
 
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
+{
+  path: '/example',
+  component: Layout,
+  redirect: '/example/table',
+  name: 'Example',
+  meta: {
+    title: 'Example',
+    icon: 'el-icon-s-help'
+  },
+  children: [{
+    path: 'table',
+    name: 'Table',
+    component: () => import(/* webpackChunkName: "chunk-table" */ '@/views/table/index'),
     meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [{
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: {
-          title: 'Menu1'
-        },
-        children: [{
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: {
-              title: 'Menu1-1'
-            }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: {
-              title: 'Menu1-2'
-            },
-            children: [{
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: {
-                  title: 'Menu1-2-1'
-                }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: {
-                  title: 'Menu1-2-2'
-                }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: {
-              title: 'Menu1-3'
-            }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: {
-          title: 'menu2'
-        }
-      }
-    ]
+      title: 'Table',
+      icon: 'table'
+    }
   },
-
   {
-    path: 'external-link',
-    component: Layout,
-    children: [{
-      path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-      meta: {
-        title: 'External Link',
-        icon: 'link'
-      }
-    }]
-  },
-
-  // 404 page must be placed at the end !!!
-  {
-    path: '*',
-    redirect: '/404',
-    hidden: true
+    path: 'tree',
+    name: 'Tree',
+    component: () => import(/* webpackChunkName: "chunk-Tree" */ '@/views/tree/index'),
+    meta: {
+      title: 'Tree',
+      icon: 'tree'
+    }
   }
+  ]
+},
+
+{
+  path: '/nested',
+  component: Layout,
+  redirect: '/nested/menu1',
+  name: 'Nested',
+  meta: {
+    title: 'Nested',
+    icon: 'nested'
+  },
+  children: [{
+    path: 'menu1',
+    component: () => import(/* webpackChunkName: "chunk-nested" */ '@/views/nested/menu1/index'), // Parent router-view
+    name: 'Menu1',
+    meta: {
+      title: 'Menu1'
+    },
+    children: [{
+      path: 'menu1-1',
+      component: () => import(/* webpackChunkName: "chunk-Menu1-1" */ '@/views/nested/menu1/menu1-1'),
+      name: 'Menu1-1',
+      meta: {
+        title: 'Menu1-1'
+      }
+    },
+    {
+      path: 'menu1-2',
+      component: () => import(/* webpackChunkName: "chunk-Menu1-2" */ '@/views/nested/menu1/menu1-2'),
+      name: 'Menu1-2',
+      meta: {
+        title: 'Menu1-2'
+      },
+      children: [{
+        path: 'menu1-2-1',
+        component: () => import(/* webpackChunkName: "chunk-Menu1-2-1" */ '@/views/nested/menu1/menu1-2/menu1-2-1'),
+        name: 'Menu1-2-1',
+        meta: {
+          title: 'Menu1-2-1'
+        }
+      },
+      {
+        path: 'menu1-2-2',
+        component: () => import(/* webpackChunkName: "chunk-Menu1-2-2" */ '@/views/nested/menu1/menu1-2/menu1-2-2'),
+        name: 'Menu1-2-2',
+        meta: {
+          title: 'Menu1-2-2'
+        }
+      }
+      ]
+    },
+    {
+      path: 'menu1-3',
+      component: () => import(/* webpackChunkName: "chunk-Menu1-3" */ '@/views/nested/menu1/menu1-3'),
+      name: 'Menu1-3',
+      meta: {
+        title: 'Menu1-3'
+      }
+    }
+    ]
+  },
+  {
+    path: 'menu2',
+    component: () => import(/* webpackChunkName: "chunk-Menu2" */ '@/views/nested/menu2/index'),
+    name: 'Menu2',
+    meta: {
+      title: 'menu2'
+    }
+  }
+  ]
+},
+
+{
+  path: 'external-link',
+  component: Layout,
+  children: [{
+    path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+    meta: {
+      title: 'External Link',
+      icon: 'link'
+    }
+  }]
+},
+
+// 404 page must be placed at the end !!!
+{
+  path: '*',
+  redirect: '/404',
+  hidden: true
+}
 ]
 
 const createRouter = () => new Router({
